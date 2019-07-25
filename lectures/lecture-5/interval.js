6be76d7823fe8391e85f773977e7b787ede63cc1
jungkumseok <jungkumseok@gmail.com>
2019-07-25 10:41:55 -0700

    updated lecture-5 activity 1 js

--- lectures/lecture-5/interval.js
+++ lectures/lecture-5/interval.js
@@ -1,23 +1,9 @@
 // Solution to the class activity using setItnerval
 
-var invokeTimes = function(func, noTimes, time) {
-    console.log("Setting up interval " + noTimes + " " + time);
-    var count = 0;
-    var interval;
-    var intervalHandler = function() {
-	console.log( "invocation " + count);
-	func(count);
-	count = count + 1;
-	if (count == noTimes) {
-	    clearInterval(interval);
-	}
-    };
-    if (noTimes >0)
-	interval = setInterval(intervalHandler, time);
+var invokeTimes = function(func, num, time) {
+    // to implement
 };
 
-var setup = function() {
-    invokeTimes( function(i) { alert("hello " + i); }, 10, 1000 );
-}
-
-setup();
+invokeTimes( function(count) { 
+      console.log("Hello " + count);
+   }, 10, 1000 );
